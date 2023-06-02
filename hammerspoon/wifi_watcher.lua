d5b3f3ae08bbe280fb98815469c99c06b99b2e27
Alex Mülli <amuelli@gmail.com>
2023-06-02 10:01:10 +0200

    update hammerspoon wifi watcher

--- hammerspoon/wifi_watcher.lua
+++ hammerspoon/wifi_watcher.lua
@@ -1,14 +1,22 @@
-wifiWatcher = nil
-function ssidChanged()
-local wifiName = hs.wifi.currentNetwork()
-if wifiName then
-wifiMenu:setTitle(wifiName)
-else 
-wifiMenu:setTitle("Wifi OFF")
-end
-end
+wifiMenu = hs.menubar.new(true, 'Wifi SSID')
+--                    ^   ^     ^-- autosaveName (makes the menu stay in the same place across Hammerspoon launches)
+--                    |   |         This can be whatever you want.
+--                    |   |         It just controls the name for the preference where the position is saved.
+--                    |   `-------- Start out showing it in the menubar
+--                    |             (default, but needed to get autosaveName into the right argument)
+--                    `------------ Use new() instead of newWithPriority()
+-- Everything from here on is the same as what you have now:
+wifiMenu:setTitle(hs.wifi.currentNetwork())
 
-wifiMenu = hs.menubar.newWithPriority(2147483645)
-ssidChanged()
+wifiWatcher = nil
+
+function ssidChanged()
+    local wifiName = hs.wifi.currentNetwork()
+    if wifiName then
+        wifiMenu:setTitle(wifiName)
+    else
+        wifiMenu:setTitle("Wifi OFF")
+    end
+end
 
 wifiWatcher = hs.wifi.watcher.new(ssidChanged):start()
